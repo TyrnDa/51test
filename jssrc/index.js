6b9fa934cbc5c6c2796656aab8d8177ec0c74491
zhuweifeng <weifeng.zhu@51auto.com>
2016-04-29 10:59:56 +0800

    first com

---
+++ jssrc/index.js
@@ -0,0 +1,19 @@
+$(function () {
+    /*var fade = function (node) {
+        var leval = 1;
+        var step = function () {
+            var hex = leval.toString(16);
+            node.style.backgroundColor = '#FFFF' + hex + hex;
+
+            console.log('#FFFF' + hex + hex);
+            if(leval < 15){
+                leval += 1;
+                setTimeout(step, 100);
+            }
+        };
+        setTimeout(step, 100);
+    };
+
+    fade(document.body);*/
+    var a = 0;
+});
